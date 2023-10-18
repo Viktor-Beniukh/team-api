7e90ef0a462ea9a101ae457e26fc71abb9f0102e
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-10-18 19:00:27 +0300

    Added models for team app

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    team = models.ForeignKey(
+        Team, on_delete=models.CASCADE, related_name="members", null=True, blank=True
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
