6ee4c000647de9bddc45ee70c30657e0dd998aa9
Viktor Beniukh <superbigbenjamin@gmail.com>
2023-10-19 15:42:12 +0300

    Added Swagger

--- team_service/urls.py
+++ team_service/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("teams.urls", namespace="api")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
